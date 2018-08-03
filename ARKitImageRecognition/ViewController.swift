e9531a78cba8bcc4274d55c2728ac4f277f5f1ba
Federico Nieto <federico@lateralview.net>
2018-08-03 15:49:28 -0300

    Added Mano Ojo image reference and video played when recognized

--- ARKitImageRecognition/ViewController.swift
+++ ARKitImageRecognition/ViewController.swift
@@ -107,6 +107,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             // Add the plane visualization to the scene.
             node.addChildNode(planeNode)
+            
+            // Display Video
+            if referenceImage.name == "HandEye" {
+                self.displayVideo()
+            }
+
         }
 
         DispatchQueue.main.async {
@@ -125,5 +131,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             .fadeOut(duration: 0.5),
             .removeFromParentNode()
         ])
+    }
+    
+    func displayVideo() {
+        guard let currentFrame = self.sceneView.session.currentFrame else {
+            return
+        }
+        
+        let videoNode = SKVideoNode(fileNamed: "MANO_OJO.mov")
+        videoNode.play()
+        
+        let skScene = SKScene(size: CGSize(width: 640, height: 480))
+        skScene.addChild(videoNode)
+        
+        
+        videoNode.position = CGPoint(x: skScene.size.width/2, y: skScene.size.height/2)
+        videoNode.size = skScene.size
+        
+        let tvPlane = SCNPlane(width: 1.0, height: 0.75)
+        tvPlane.firstMaterial?.diffuse.contents = skScene
+        tvPlane.firstMaterial?.isDoubleSided = true
+        
+        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.0
+        
+        tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        tvPlaneNode.eulerAngles = SCNVector3(Double.pi,0,0)
+        
+        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
     }
 }
